904ec567c17b2af65ab970010a204a75854d3c69
kishore <91112126+kishorerio@users.noreply.github.com>
2024-09-03 11:16:49 +0530

    Delete src/helpers directory

--- src/helpers/jwt-token-access/accessToken.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable prettier/prettier */
-function getAccessToken() {
-  const token = JSON.parse(sessionStorage.getItem('accessToken'))
-  return token ? `Bearer ${token}` : null
-}
-
-export default getAccessToken
--- src/helpers/url_helper.js
+++
@@ -1,6 +0,0 @@
-/* eslint-disable prettier/prettier */
-//LOGIN
-export const POST_FAKE_LOGIN = '/auth/login';
-
-export const ADMIN_LOGIN = 'admin/login';
-export const LIST_OF_USERS = 'get/user/list';
